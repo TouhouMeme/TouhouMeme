e6807367c4d45f94008a7470656332961b1c97a2
shirokurakana <shirokurakana@users.noreply.github.com>
2025-06-27 11:48:12 +0000

    chore: update config file

--- static/scripts/config.js
+++ static/scripts/config.js
@@ -1,5 +1,12 @@
 export default {
   items: [
+    "meme/《睡觉》.jpg",
+    "meme/刚刚群里出现了复读的异变！这要 怎么解决这次异变呢？.jpg",
+    "meme/名侦探囧仙.jpg",
     "meme/哇多么好的机会啊.jpg",
+    "meme/妈个陀螺仪的.jpg",
+    "meme/稍等我接个电话.jpg",
+    "meme/羡慕极了.jpg",
+    "meme/让人有点嫉妒呢.jpg",
   ]
 }
