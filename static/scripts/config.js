d621cf86828df5aee5afa4c390f5e31a95a77a2d
shirokurakana <shirokurakana@users.noreply.github.com>
2025-06-27 12:07:52 +0000

    chore: update config file

--- static/scripts/config.js
+++ static/scripts/config.js
@@ -1,10 +1,18 @@
 export default {
   items: [
+    "meme/15岁女孩为何长白发.jpeg",
+    "meme/《天子之气》.jpeg",
+    "meme/《娘化》.png",
+    "meme/《看亲友打》.jpeg",
     "meme/《睡觉》.jpg",
+    "meme/不可以两边一起啦.jpeg",
+    "meme/你在期待什么.jpeg",
     "meme/刚刚群里出现了复读的异变！这要 怎么解决这次异变呢？.jpg",
     "meme/名侦探囧仙.jpg",
     "meme/哇多么好的机会啊.jpg",
     "meme/妈个陀螺仪的.jpg",
+    "meme/我他妈射爆.jpeg",
+    "meme/换个头试试.jpeg",
     "meme/稍等我接个电话.jpg",
     "meme/羡慕极了.jpg",
     "meme/让人有点嫉妒呢.jpg",
